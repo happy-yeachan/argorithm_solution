bf1d314360ece5a23c7ab15330138538d2431a0f
yeachan <dus001228@naver.com>
2023-01-24 19:10:12 +0900

    코테_괄호회전

--- 코테_괄호회전하기.py
+++ 코테_괄호회전하기.py
@@ -1,19 +1,32 @@
-import re
 def solution(s):
-    s = list(s)
-    sum = 0
-    if (len(s)%2 ==1):
+    s = list(s)  #문자열을 리스트화
+    sum = len(s)
+    if (len(s)%2 ==1):  #홀수이면 올바른 괄호 문자열이 안됨 -> 미리 판별
         return 0
     for i in range(0, len(s)):
-        test = ''.join(s)
-        # test = test.replace("()", "").strip()
-        # test = test.replace("{}", "").strip()
-        # test = test.replace("[]", "").strip()
-        new_text = re.sub("[]", "", test)
-        print(new_text)
-        if (len(new_text) == 0):
-            sum += 1
+        tmp = [] # 괄호의 짝을 판별할 스택 구현
+        try:
+            for j in range(0, len(s)):
+                if(s[j] == ")"):
+                    y = tmp.pop()
+                    if(y == "("):
+                        continue
+                    else:
+                        sum -= 1
+                        break
+                elif(s[j] == "}" or s[j] == "]"):
+                    y = tmp.pop()
+                    if(ord(y)+2 == ord(s[j])):  #아스키코드로 한번에 판별
+                        continue
+                    else:
+                        sum -= 1
+                        break
+                else:
+                    tmp.append(s[j])
+        except IndexError:
+            sum -= 1
+
         s.append(s.pop(0))
     return sum
 
-print(solution('[](){}'))
+print(solution('}]()[{'))
