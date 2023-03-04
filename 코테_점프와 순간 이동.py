fd2feb7767adb7f42f523f9f2c0bac27776214f9
yeachan <dus001228@naver.com>
2023-03-04 14:33:05 +0900

    3/4

---
+++ 코테_점프와 순간 이동.py
@@ -0,0 +1,12 @@
+def solution(n):
+    cnt = 1
+    while(n!=1):
+        if n%2 == 0:
+            n /= 2
+        else:
+            n = (n-1)/2
+            cnt += 1
+    return cnt
+
+
+print(solution(5))
