bbbea5bed1280c9472bb086fa2d085d5c08c9a46
예찬예찬 <dus001228@naver.com>
2023-07-10 10:30:38 +0900

    Update 강의실 배정.py

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,15 +1,21 @@
 import sys, heapq
-# 입력
-n = int(sys.stdin.readline())
+
 classT = []
 classR = []
 
+# 입력
+n = int(sys.stdin.readline())
 for i in range(n): classT.append(list(map(int, sys.stdin.readline().split())))
 
+# 수업 시작 시간을 기준으로 정렬
 classT.sort(key = lambda x:x[0])
+
+# 가장 먼저 시작하는 수업 푸쉬
+# 힙큐를 사용하여 자동으로 정렬되게 설정
 heapq.heappush(classR, classT[0][1])
 
 for i in range(1, n):
+    # 수업이 끝난 강의실이 있으면 거기 사용
     if classR[0] <= classT[i][0]:
         heapq.heappop(classR)
         heapq.heappush(classR, classT[i][1])
