d900bf68cf6d4e7f8c160724deca5fa65b7f8a6c
예찬예찬 <dus001228@naver.com>
2023-09-06 23:22:44 +0900

    [Gold IV] Title: 연구소, Time: 1352 ms, Memory: 118796 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,57 +1,60 @@
-from collections import deque
-from sys import stdin
-def bfs(matrixx):    
-    q=deque()        
-    for x in virus:
-        q.append(x)
-    dx=[-1,1,0,0]    
-    dy=[0,0,1,-1]
-    while q:
-        r,c=q.popleft()        
-        for x in range(4):
-            nr=r+dx[x]
-            nc=c+dy[x]
-            if nr>=n or nr<0 or nc>=m or nc<0:
-                continue
-            if matrixx[nr][nc]==2 or matrixx[nr][nc]==1:            
-                continue            
-            matrixx[nr][nc]=2
-            q.append([nr,nc])
-    a=checkSafe(matrixx)
-    return a
+import sys
+input = sys.stdin.readline
 
-            
-def checkSafe(matrixxx):
-    cnt=0
-    for x in range(n):
-        for y in range(m):
-            if matrixxx[x][y]==0:
-                cnt+=1
-    return cnt                                
-    
-input=stdin.readline
-n,m=map(int,input().split())
-safe=[]
-matrix=[list(map(int,input().split())) for _ in range(n)]
-virus=[]
-
-for x in range(n):
-    for y in range(m):
-        if matrix[x][y]==0:
-            safe.append([x,y])
-        if matrix[x][y] == 2:
-            virus.append([x,y])
-answer=0                  
-import copy                     
-def back(depth,idx):
-    global answer    
-    if depth == 3: 
-        tmp=[mat[:] for mat in matrix]            
-        answer=max(bfs(tmp),answer)                
+# 백트래킹
+def back(wall):
+    if wall == 3:
+        tmp_li = [mat[:] for mat in li]
+        bfs(tmp_li)
         return
-    for x in range(idx,len(safe)):
-        matrix[safe[x][0]][safe[x][1]]=1
-        back(depth+1,x+1)
-        matrix[safe[x][0]][safe[x][1]]=0
-back(0,0)
-print(answer)
+
+    for i in range(n):
+        for j in range(m):
+            if li[i][j] == 0:
+                li[i][j] = 1
+                back(wall+1)
+                li[i][j] = 0
+
+dy = [0, -1, 0, 1]
+dx = [-1, 0, 1, 0]
+
+# bfs진행
+from collections import deque
+def bfs(tmp_li):
+    global result
+    q = deque()
+
+    for i in range(n):
+        for j in range(m):
+            if li[i][j] == 2:
+                q.append((i,j))
+
+    while q:
+        y, x = q.popleft()
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0<= ny <n and 0<=nx<m:
+                if tmp_li[ny][nx] == 0:
+                    tmp_li[ny][nx] = 2
+                    q.append((ny, nx))
+    count = check_safe(tmp_li)
+    result = max(result, count)
+
+def check_safe(tmp_li):
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if tmp_li[i][j] == 0:
+                cnt += 1
+    return cnt
+
+
+n, m = list(map(int, input().split()))
+li = [list(map(int, input().split())) for _ in range(n)]
+
+result = 0
+
+back(0)
+
+print(result)
