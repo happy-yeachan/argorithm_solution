a2a88af859dd82919aac7eeee7e24660c1f014b6
예찬예찬 <dus001228@naver.com>
2023-09-19 16:29:46 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -4,19 +4,16 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-li = []
-for i in range(n):
-    a = int(input())
-    li.append(a)
+li = [ int(input()) for _ in range(n)]
+
 n -= 1
 cnt = 0
 while k != 0:
     if li[n] > k:
         n -=1
     else:
-        div = k//li[n]
-        k -= li[n]*(div)
-        cnt += div
+        cnt += k // li[n]
+        k %= li[n]
 
 
 print(cnt)
