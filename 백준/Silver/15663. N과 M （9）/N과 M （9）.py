15dafa4683d320eb0967070dd00493c65b3427b8
예찬예찬 <dus001228@naver.com>
2023-09-20 16:30:21 +0900

    [Silver II] Title: N과 M (9), Time: 96 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+def dfs():
+    if len(temp) == m:
+        print(*temp)
+        return
+    remember_me = 0
+    for i in range(n):
+        if not visited[i] and remember_me != nums[i]:
+            visited[i] = True
+            temp.append(nums[i])
+            remember_me = nums[i]
+            dfs()
+            visited[i] = False
+            temp.pop()
+
+n, m = map(int, input().split())
+nums = sorted(list(map(int, input().split())))
+visited = [False] * n
+temp = []
+
+dfs()
