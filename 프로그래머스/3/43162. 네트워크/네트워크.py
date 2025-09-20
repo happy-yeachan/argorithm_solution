e5f923d3b62d6cbffcc0b4bdfa89ba274c5a56f4
YEACHAN OH <dus001228@naver.com>
2025-09-20 11:36:00 +0900

    [level 3] Title: 네트워크, Time: 0.49 ms, Memory: 9.33 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,21 @@
+def solution(n, computers):
+    global chk
+    answer = 0
+    chk = [False]*n
+    for i in range(n):
+        if not chk[i]:
+            chk[i] = True
+            answer += 1
+            dfs(i, computers, n)
+    return answer
+
+from collections import deque
+def dfs(idx, computers, n):
+    q = deque()
+    q.append(idx)
+    while q:
+        now = q.popleft()
+        for i in range(idx+1, n):
+            if computers[now][i] == 1 and not chk[i]:
+                chk[i] = True
+                q.append(i)
